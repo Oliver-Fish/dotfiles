37a354fcf410c60a3a0848acfce5005487b62dd6
Oliver Fish <git@oliver.fish>
2017-11-18 08:46:48 +0000

    Added i3 and Polybar configs

--- setup.sh
+++ setup.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+
 #Build Symlinks
 ln -sf ~/.dotfiles/.zshrc ~/.zshrc
 ln -sf ~/.dotfiles/.vimrc ~/.vimrc
+ln -s ~/.dotfiles/i3 ~/.config/i3
+ln -s ~/.dotfiles/polybar ~/.config/polybar
