d145aeac55ee22f96db610c9cd424e4c476f9e45
Oliver Fish <github@oliver.fish>
2018-03-28 13:01:39 +0200

    Added symlink for .zshenv to setup file

--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@
 
 #Build Symlinks
 ln -sf ~/.dotfiles/.zshrc ~/.zshrc
+ln -sf ~/.dotfiles/.zshenv ~/.zshenv
 ln -sf ~/.dotfiles/.vimrc ~/.vimrc
 ln -sf ~/.dotfiles/.Xresources ~/.Xresources 
 ln -s ~/.dotfiles/i3 ~/.config/i3
