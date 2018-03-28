732424b9daeb679f4c8df109731c32d8b1fccc0e
Oliver Fish <github@oliver.fish>
2018-03-28 13:03:39 +0200

    Added config fiele for dunstrc (notifications)

--- setup.sh
+++ setup.sh
@@ -5,6 +5,7 @@ ln -sf ~/.dotfiles/.zshrc ~/.zshrc
 ln -sf ~/.dotfiles/.zshenv ~/.zshenv
 ln -sf ~/.dotfiles/.vimrc ~/.vimrc
 ln -sf ~/.dotfiles/.Xresources ~/.Xresources 
+ln -sf ~/.dotfiles/.config/dunstrc ~/.config/dunstrc 
 ln -s ~/.dotfiles/i3 ~/.config/i3
 ln -s ~/.dotfiles/polybar ~/.config/polybar
 ln -s ~/.vimrc ~/.config/nvim/init.vim
